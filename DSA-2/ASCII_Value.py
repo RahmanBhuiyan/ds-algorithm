2a8b75eb1654b3084743a166a28a67323a7d79fb
RahmanBhuiyan <abdullahalrahman2002@gmail.com>
2021-03-07 23:48:04 +0600

    hash table

---
+++ DSA-2/ASCII_Value.py
@@ -0,0 +1,16 @@
+value=input("input your data = ")
+list=[None]*10
+j=10
+ok=0
+for i in value:
+    b=ord(i)
+    # print(b)
+    ok=ok+b
+chill=ok%j
+print(chill)
+
+def insert(self,chill,value):
+    hash_key=chill
+    list[hash_key]=value
+    return list
+print(insert(chill,value))
---
+++ DSA-2/hash_table.py
@@ -0,0 +1,46 @@
+class hashtable:
+    def __init__(self):
+        self.max=100
+        self.list=[None]*self.max
+    def hash_function(self,key):
+        ok=0
+        for i in key:
+            b=ord(i)
+            ok=ok+b
+        chill=ok%self.max
+        return chill
+    def insert (self,key,val):
+        ok=self.hash_function(key)
+        self.list[ok]=val
+    def get (self,key):
+        ok=self.hash_function(key)
+        return self.list[ok]
+    def delate(self,key):
+        # delate iteam in the hashtable
+        ok=self.hash_function(key)
+        self.list[ok]=None
+t=hashtable()
+t.insert("rahman",450)
+t.insert("error 4044","01869019351")
+print(t.get("rahman"))
+print(t.get("error 4044"))
+print(t.list)
+
+'''
+#nonetype error how to solve 
+
+# def demo():
+# 	print(3+4)
+# print('printed fucntion return none\n', demo())
+
+# # n = demo()+3
+# # print(n) #this will produce error.
+
+
+# def demo1():
+# 	return 3+4
+# print('returned function returns the value\n',demo1())
+
+# n1 = demo1()+3
+# print(n1)
+'''
