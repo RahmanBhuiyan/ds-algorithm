fb0190cbac3d7bd8755115206eb8ef40e9a6f2bd
Rahman Bhuiyan <65069133+RahmanBhuiyan@users.noreply.github.com>
2021-03-26 21:16:42 +0600

    edite code

--- DSA_intro/Fibonacci_numbers.py
+++ DSA_intro/Fibonacci_numbers.py
@@ -1,28 +1,15 @@
-import time
-{
-startime=time.time()
 number=int(input("input your number = "))
-}=z
-{
-a=0 
-b=1
-}=z
-print(a)
-print(b)
+def fibonacci(number):
+    a=0 
+    b=1
+    print(a)
+    print(b)
 
-# print(end="")
-for i in range(number):=n+1
-    c=a+b
-    #transfer value a to b and b to c
-    a=b
-    b=c
-    print(c) =n
-    
-print(time.time()-startime)
-z=1
-f(n)=z+z+n+1
-    =2*1+n+1
-    =3+n
-    =n 
-    o(n)
-      
+    # print(end="")
+    for i in range(number):=n+1
+        c=a+b
+        #transfer value a to b and b to c
+        a=b
+        b=c
+        return c
+print(fibonacci(number))
