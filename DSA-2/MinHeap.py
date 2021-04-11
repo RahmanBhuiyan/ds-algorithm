257480876f8c8433361143a082fd121f813e3a1e
RahmanBhuiyan <abdullahalrahman2002@gmail.com>
2021-04-11 12:08:20 +0600

    heap dsa

--- DSA-2/MinHeap.py
+++ DSA-2/MinHeap.py
@@ -19,10 +19,17 @@ class MinHeap:
         return 2*self.parent(index)+1
     def Rightchild(self,index):
         return 2*self.parent(index)+2
+    def hasleftchild(self,index):
+        return self.Rleftchild(index) < self.size
+    def hasrightchild(self,index):
+        return self.RrightChild(index) < self.size
     def aSwap(self,index):
         if self.hasparent(index) and self.storage[self.leftchild(index)] > self.storage[self.Rightchild(index)]:
             self.storage[self.leftchild(index)],self.storage[self.Rightchild(index)]=self.storage[self.Rightchild(index)],self.storage[self.leftchild(index)]
-
+    def Rleftchild (self,index):
+        return index*2+1
+    def RrightChild(self,index):
+        return index*2+2
     def insert(self,data):
         if(self.storage_full()):
             return "Heap is full"
@@ -36,12 +43,40 @@ class MinHeap:
             self.swap(self.parent(index),index)
             index=self.parent(index)
             self.aSwap(index)
+    def remove(self):
+        if (self.size==0):
+            return "heap is empty"
+        data=self.storage[0]
+        self.storage[0]=self.storage[self.size-1]
+        self.size-=1
+        # return self.storage.remove(self.storage[self.size])
+        # self.storage.remove([self.size-=1])
+        self.heapdown()
+        return data  
+    def heapdown(self):
+        index=0
+        while self.hasleftchild(index):
+            child=self.Rleftchild(index)
+            if self.hasrightchild(index) and (self.storage[self.RrightChild(index)] < self.storage[self.Rleftchild(index)]):
+                child=self.RrightChild(index)
+            if self.storage[index] < self.storage[child]  :
+                #parent < child 
+                break
+            else:
+                self.swap(index,child)
+            index=child
+        self.storage.remove(self.storage[self.size])
+
 
 obj=MinHeap(10)
-obj.insert(3)
-obj.insert(9)
-obj.insert(2)
-obj.insert(1)
-obj.insert(4)
+obj.insert(0)
 obj.insert(5)
+obj.insert(10)
+obj.insert(20)
+obj.insert(8)
+obj.insert(15)
+obj.insert(30)
+print(obj.remove())
+print(obj.remove())
+print(obj.size)
 print(obj.storage)
--- DSA_intro/Fibonacci_numbers.py
+++ DSA_intro/Fibonacci_numbers.py
@@ -1,28 +1,22 @@
-import time
-{
-startime=time.time()
 number=int(input("input your number = "))
-}=z
-{
 a=0 
 b=1
-}=z
 print(a)
 print(b)
 
 # print(end="")
-for i in range(number):=n+1
+for i in range(number):
     c=a+b
     #transfer value a to b and b to c
     a=b
     b=c
-    print(c) =n
+    print(c) 
     
-print(time.time()-startime)
-z=1
-f(n)=z+z+n+1
-    =2*1+n+1
-    =3+n
-    =n 
-    o(n)
+# print(time.time()-startime)
+# z=1
+# f(n)=z+z+n+1
+#     =2*1+n+1
+#     =3+n
+#     =n 
+#     o(n)
       
