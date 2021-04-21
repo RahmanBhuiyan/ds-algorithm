db158e572639f35afc8b3a07eeab6a2ba5905e77
RahmanBhuiyan <abdullahalrahman2002@gmail.com>
2021-04-21 11:25:28 +0600

    solve perfact binary tree

--- TREE_DSA-3/Perfect_Binary_Tree.py
+++ TREE_DSA-3/Perfect_Binary_Tree.py
@@ -1,27 +1,38 @@
+# Checking if a binary tree is a perfect binary tree in Python
+
+
 class newNode:
     def __init__(self, k):
         self.key = k
         self.right = self.left = None
 
+
+# Calculate the depth
 def calculateDepth(node):
     d = 0
     while (node is not None):
         d += 1
-        node = node.left 
+        node = node.left
     return d
-def Depth(node):
-    d = 0
-    while (node is not None):
-        d += 1
-        node = node.right 
-    return d 
 
-def chill(node):
-    d= calculateDepth(node) + Depth(node)
-    if d%2==0:
-        print("not full binary tree")
-    else:
-        print("full binary tree")
+
+# Check if the tree is perfect binary tree
+def is_perfect(root, d, level=0):
+
+    # Check if the tree is empty
+    if (root ):
+        # return True
+
+        # Check the presence of trees
+        if (root.left is None and root.right is None):
+            return (d == level + 1)
+
+        if (root.left is None or root.right is None):
+            return False
+
+        return (is_perfect(root.left, d, level + 1) and
+                is_perfect(root.right, d, level + 1))
+
 
 root = None
 root = newNode(1)
@@ -29,9 +40,9 @@ root.left = newNode(2)
 root.right = newNode(3)
 root.left.left = newNode(4)
 root.left.right = newNode(5)
-root.left.left.left = newNode(40)    
-
-# calculateDepth(root)
-# print()
-# Depth(root)
-print(chill(root))
+root.right.left = newNode(7)
+root.right.right = newNode(9)
+if (is_perfect(root, calculateDepth(root))):
+    print("The tree is a perfect binary tree")
+else:
+    print("The tree is not a perfect binary tree")
