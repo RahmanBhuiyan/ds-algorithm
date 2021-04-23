08c6d6ef5468af8d786f5958d53456aa7fcd51e0
jahir-raihan <78207450+jahir-raihan@users.noreply.github.com>
2021-04-23 19:43:22 +0600

    SinglyLinkedList
    I've added an method to add values in LinkedList Node.

--- oop/single_linkedlist.py
+++ oop/single_linkedlist.py
@@ -6,6 +6,17 @@ class linkedlist(Node):
     def __init__(self):
         # super().__init__(data)
         self.head=None
+        
+    def insert_at_end(self, data):
+        if self.head is None:
+            node = Node(data, self.head)
+            self.head = node
+            return
+        itrator = self.head
+        while itr.next:
+            itrator = itrator.next
+        itrator.next = Node(data, None)
+        
     def print_link(self):
         if self.head == None :
             print("linked list is empty")
